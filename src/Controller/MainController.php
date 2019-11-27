7bc5ed4a2ea5af5a8ff1508f8ba763bfb1724e54
Piotr Popiołek <piotr.popiolek93@gmail.com>
2019-11-27 09:43:37 +0100

    Main

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,10 +8,15 @@ class MainController
 {
     public function index()
     {
-        $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+        $response = Unirest\Request::get(
+            "https://coingecko.p.rapidapi.com/coins/markets?page=1&per_page=100&order=market_cap_desc&vs_currency=usd",
+            array(
+                "X-RapidAPI-Host" => "coingecko.p.rapidapi.com",
+                "X-RapidAPI-Key" => "002a3e6fd9msh804451a0819bffep107d46jsnd06912c8e7d7"
+            )
         );
+
+        return $response;
     }
 }
